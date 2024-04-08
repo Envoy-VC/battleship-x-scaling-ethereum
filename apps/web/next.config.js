d544375e51ae4f60d1b71368217267a1a65d3df7
Vedant Chainani <vedantchainani1084@gmail.com>
2024-04-09 04:58:57 +0530

    chore: add shadcn and nillion config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -7,6 +7,17 @@ const config = {
     locales: ['en'],
     defaultLocale: 'en',
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
+          { key: 'Cross-Origin-Embedder-Policy', value: 'require-corp' },
+        ],
+      },
+    ];
+  },
 };
 
 export default config;
