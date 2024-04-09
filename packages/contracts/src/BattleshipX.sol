3bbb399a9700529fc9626a811b7023594687f15d
Vedant Chainani <vedantchainani1084@gmail.com>
2024-04-09 05:48:36 +0530

    chore: add basic tests and remappings

--- packages/contracts/src/BattleshipX.sol
+++ packages/contracts/src/BattleshipX.sol
@@ -2,37 +2,32 @@
 
 pragma solidity ^0.8.21;
 
-import {RMRKAbstractNestableMultiAsset} from "@rmrk-team/evm-contracts/contracts/implementations/abstract/RMRKAbstractNestableMultiAsset.sol";
+import {RMRKAbstractNestableMultiAsset} from
+    "@rmrk-team/evm-contracts/contracts/implementations/abstract/RMRKAbstractNestableMultiAsset.sol";
 import {RMRKTokenURIPerToken} from "@rmrk-team/evm-contracts/contracts/implementations/utils/RMRKTokenURIPerToken.sol";
-import {RMRKImplementationBase} from "@rmrk-team/evm-contracts/contracts/implementations/utils/RMRKImplementationBase.sol";
-
+import {RMRKImplementationBase} from
+    "@rmrk-team/evm-contracts/contracts/implementations/utils/RMRKImplementationBase.sol";
 
 contract BattleshipX is RMRKAbstractNestableMultiAsset, RMRKTokenURIPerToken {
-    // Events 
+    // errors
+    error RMRKWrongValueSent();
+    // Events
     // Variables
+
     uint256 private _pricePerMint;
     mapping(address => bool) private _autoAcceptCollection;
 
     // Constructor
     constructor(
-      string memory collectionMetadata,
-      uint256 maxSupply,
-      address royaltyRecipient,
-      uint16 royaltyPercentageBps,
-      uint256 pricePerMint_
-    )
-        RMRKImplementationBase(
-            "MyNFT2",
-            "NFT2",
-            collectionMetadata,
-            maxSupply,
-            royaltyRecipient,
-            royaltyPercentageBps
-        )
-    {
-      _pricePerMint = pricePerMint_;
-}
-    
+        string memory collectionMetadata,
+        uint256 maxSupply,
+        address royaltyRecipient,
+        uint16 royaltyPercentageBps,
+        uint256 pricePerMint_
+    ) RMRKImplementationBase("MyNFT2", "NFT2", collectionMetadata, maxSupply, royaltyRecipient, royaltyPercentageBps) {
+        _pricePerMint = pricePerMint_;
+    }
+
     // Methods
     // Suggested Mint Functions
     /**
@@ -43,16 +38,11 @@ contract BattleshipX is RMRKAbstractNestableMultiAsset, RMRKTokenURIPerToken {
      * @param numToMint Number of tokens to mint
      * @return The ID of the first token to be minted in the current minting cycle
      */
-    function mint(
-        address to,
-        uint256 numToMint
-    ) public payable returns (uint256) {
-        (uint256 nextToken, uint256 totalSupplyOffset) = _prepareMint(
-            numToMint
-        );
+    function mint(address to, uint256 numToMint) public payable returns (uint256) {
+        (uint256 nextToken, uint256 totalSupplyOffset) = _prepareMint(numToMint);
         _chargeMints(numToMint);
 
-        for (uint256 i = nextToken; i < totalSupplyOffset; ) {
+        for (uint256 i = nextToken; i < totalSupplyOffset;) {
             _safeMint(to, i, "");
             unchecked {
                 ++i;
@@ -61,7 +51,7 @@ contract BattleshipX is RMRKAbstractNestableMultiAsset, RMRKTokenURIPerToken {
 
         return nextToken;
     }
-        
+
     /**
      * @notice Used to mint a desired number of child tokens to a given parent token.
      * @dev The "data" value of the "_safeMint" method is set to an empty value.
@@ -71,17 +61,11 @@ contract BattleshipX is RMRKAbstractNestableMultiAsset, RMRKTokenURIPerToken {
      * @param destinationId ID of the token into which to mint the new child token
      * @return The ID of the first token to be minted in the current minting cycle
      */
-    function nestMint(
-        address to,
-        uint256 numToMint,
-        uint256 destinationId
-    ) public payable returns (uint256) {
-        (uint256 nextToken, uint256 totalSupplyOffset) = _prepareMint(
-            numToMint
-        );
+    function nestMint(address to, uint256 numToMint, uint256 destinationId) public payable returns (uint256) {
+        (uint256 nextToken, uint256 totalSupplyOffset) = _prepareMint(numToMint);
         _chargeMints(numToMint);
 
-        for (uint256 i = nextToken; i < totalSupplyOffset; ) {
+        for (uint256 i = nextToken; i < totalSupplyOffset;) {
             _nestMint(to, i, destinationId, "");
             unchecked {
                 ++i;
@@ -90,7 +74,7 @@ contract BattleshipX is RMRKAbstractNestableMultiAsset, RMRKTokenURIPerToken {
 
         return nextToken;
     }
-    
+
     function _chargeMints(uint256 numToMint) internal {
         uint256 price = numToMint * _pricePerMint;
         if (price != msg.value) revert RMRKWrongValueSent();
@@ -120,72 +104,58 @@ contract BattleshipX is RMRKAbstractNestableMultiAsset, RMRKTokenURIPerToken {
      * @param _amount The amount to withdraw
      */
     function _withdraw(address _address, uint256 _amount) private {
-        (bool success, ) = _address.call{value: _amount}("");
+        (bool success,) = _address.call{value: _amount}("");
         require(success, "Transfer failed.");
     }
-    
-    function setAutoAcceptCollection(
-        address collection,
-        bool autoAccept
-    ) public virtual onlyOwnerOrContributor {
+
+    function setAutoAcceptCollection(address collection, bool autoAccept) public virtual onlyOwnerOrContributor {
         _autoAcceptCollection[collection] = autoAccept;
     }
 
-    function _afterAddChild(
-        uint256 tokenId,
-        address childAddress,
-        uint256 childId,
-        bytes memory
-    ) internal virtual override {
+    function _afterAddChild(uint256 tokenId, address childAddress, uint256 childId, bytes memory)
+        internal
+        virtual
+        override
+    {
         // Auto accept children if they are from known collections
         if (_autoAcceptCollection[childAddress]) {
-            _acceptChild(
-                tokenId,
-                _pendingChildren[tokenId].length - 1,
-                childAddress,
-                childId
-            );
+            _acceptChild(tokenId, _pendingChildren[tokenId].length - 1, childAddress, childId);
         }
     }
-    
-    function mintWithAssets(
-        address to,
-        uint64[] memory assetIds
-    ) public virtual onlyOwnerOrContributor {
-        (uint256 tokenId, ) = _prepareMint(1);
+
+    function mintWithAssets(address to, uint64[] memory assetIds) public virtual onlyOwnerOrContributor {
+        (uint256 tokenId,) = _prepareMint(1);
         _safeMint(to, tokenId, "");
-        for (uint256 i; i < assetIds.length; ) {
+        for (uint256 i; i < assetIds.length;) {
             _addAssetToToken(tokenId, assetIds[i], 0);
             // Only first asset or assets added by token owner are auto-accepted, so we might need to accept for the rest of cases
             if (_pendingAssets[tokenId].length != 0) {
-                _acceptAsset(tokenId, 0 , assetIds[i]);
+                _acceptAsset(tokenId, 0, assetIds[i]);
             }
             unchecked {
                 ++i;
             }
         }
     }
-    
-    function nestMintWithAssets(
-        address parentContract,
-        uint256 destinationId,
-        uint64[] memory assetIds
-    ) public virtual onlyOwnerOrContributor {
-        (uint256 tokenId, ) = _prepareMint(1);
+
+    function nestMintWithAssets(address parentContract, uint256 destinationId, uint64[] memory assetIds)
+        public
+        virtual
+        onlyOwnerOrContributor
+    {
+        (uint256 tokenId,) = _prepareMint(1);
         uint256 length = assetIds.length;
         _nestMint(parentContract, tokenId, destinationId, "");
-        for (uint256 i; i < length; ) {
+        for (uint256 i; i < length;) {
             uint64 assetId = assetIds[i];
             _addAssetToToken(tokenId, assetId, 0);
             // Only first asset or assets added by token owner are auto-accepted, so we might need to accept for the rest of cases
             if (_pendingAssets[tokenId].length != 0) {
-                _acceptAsset(tokenId, 0 , assetIds[i]);
+                _acceptAsset(tokenId, 0, assetIds[i]);
             }
             unchecked {
                 ++i;
             }
         }
     }
-    
 }
-  
--- packages/contracts/test/BattleshipX.t.sol
+++ packages/contracts/test/BattleshipX.t.sol
@@ -1,13 +1,22 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
+
+import {VmSafe} from "forge-std/Vm.sol";
 import {BattleshipX} from "../src/BattleshipX.sol";
 
 contract CounterTest is Test {
     BattleshipX public battleship;
+    VmSafe.Wallet public owner;
 
     function setUp() public {
-        battleship = new BattleshipX();
+        owner = vm.createWallet("owner");
+        battleship = new BattleshipX("", 10000, owner.addr, 0, 0);
+        console2.log(address(battleship));
+    }
+
+    function testCreateGame() public {
+        console2.log(address(battleship));
     }
 }
