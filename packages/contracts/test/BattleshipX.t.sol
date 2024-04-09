8d4828608e2c391847269e438d4b49403274e6c3
Vedant Chainani <vedantchainani1084@gmail.com>
2024-04-09 05:36:16 +0530

    chore: add initial nestable nft contracts

---
+++ packages/contracts/test/BattleshipX.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {BattleshipX} from "../src/BattleshipX.sol";
+
+contract CounterTest is Test {
+    BattleshipX public battleship;
+
+    function setUp() public {
+        battleship = new BattleshipX();
+    }
+}
