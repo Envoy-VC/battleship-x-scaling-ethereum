17751613efd14c4fc4a06f4be7767cb7be1ced17
Vedant Chainani <vedantchainani1084@gmail.com>
2024-04-13 06:27:28 +0530

    chore: update program to take individual ship position

--- packages/nillion/programs/src/main.py
+++ packages/nillion/programs/src/main.py
@@ -3,20 +3,47 @@ from nada_dsl import *
 
 def nada_main():
     party1 = Party(name="Party1")
-    # Input the Battleship Game Row of the Opponent
-    # In format of VX - where V is value(1,2) and X is co-ordinates(0,9)
-    row = Array(SecretInteger(Input(name="row", party=party1)), size=10)
+    """
+    Input for Each Ship co-ordinates
+    
+    Coordinates for Carrier: 5
+    Coordinates for Battleship: 4
+    Coordinates for Cruiser: 3
+    Coordinates for Submarine: 3
+    Coordinates for Destroyer: 2
+    
+    Each co-ordinate is a SecretInteger of form 1XY where X is the row number and Y is the column number (0,9)
+    
+    Position is the attack co-ordinate of form 1XY where X is the row number and Y is the column number (0,9)
+    
+    
+    How attack is determined:
+    
+    For each ship we run a reduce function with accumulator as Previous Result. The reduce function returns
+    Previous Result * (position - current_value) for each value in the ship coordinates.
+    
+    So if the attack co-ordinate is present in the ship coordinates, the result will be 0 and further reduce will return 0.
+    """
+    carrier = Array(SecretInteger(Input(name="carrier", party=party1)), size=5)
+    battleship = Array(SecretInteger(
+        Input(name="battleship", party=party1)), size=4)
+    cruiser = Array(SecretInteger(Input(name="cruiser", party=party1)), size=3)
+    submarine = Array(SecretInteger(
+        Input(name="submarine", party=party1)), size=3)
+    destroyer = Array(SecretInteger(
+        Input(name="destroyer", party=party1)), size=2)
 
-    # Input the attack x co-ordinates
     position = SecretInteger(Input(name="position", party=party1))
-
-    # find if 2*10 + position is in row
 
     @nada_fn
     def check_exists(a: SecretInteger, b: SecretInteger) -> SecretInteger:
         return a * (position - b)
 
-    result = row.reduce(check_exists, Integer(1))
+    result1 = carrier.reduce(check_exists, Integer(1))
+    result2 = battleship.reduce(check_exists, result1)
+    result3 = cruiser.reduce(check_exists, result2)
+    result4 = submarine.reduce(check_exists, result3)
+    result = destroyer.reduce(check_exists, result4)
 
     out = Output(result, "out", party1)
 
